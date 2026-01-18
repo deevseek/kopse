b4a28a7bb5b73ac5ac24fd409975610a3ad4631b
deevseek <nugraha.deev@gmail.com>
2026-01-18 14:26:31 +0700

    Shorten 'Kembali ke daftar' to 'Kembali'

--- resources/views/admin/master-data/anggota/create.blade.php
+++ resources/views/admin/master-data/anggota/create.blade.php
@@ -9,7 +9,7 @@
             <svg class="h-4 w-4" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M15 18l-6-6 6-6" />
             </svg>
-            Kembali ke daftar
+            Kembali
         </a>
 
         @if ($errors->any())
